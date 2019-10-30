4f87ac8ea813ee13f67c8e1777cd4c65e6c6261d
zhiqiangxu <652732310@qq.com>
2019-10-30 08:47:42 +0800

    comment

--- codegen/main.go
+++ codegen/main.go
@@ -5,15 +5,21 @@ import (
 	"github.com/zhiqiangxu/qrpc/codegen"
 )
 
-//请在generated目录运行该main函数
+//请在generated目录运行该main函数，
+//该main函数会将Client和Server代码生成到generated目录
+//然后便可运行run_test中的TestCGPerformance
 func main() {
 
+	// 声明目标服务
 	var s service.Service
 	g := codegen.New("demo")
 
+	// 自动注册s的方法
 	g.Register(&s)
 
+	// 生成代码，缓存到内存
 	g.Generate()
 
+	// 将生成的代码输出到Cwd
 	g.Output()
 }
